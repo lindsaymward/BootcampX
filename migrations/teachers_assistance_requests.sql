2bacb594810cb678bb7543ccc79d0e175ab52b0d
Lindsay Ward <lward23@hotmail.com>
2023-05-30 13:38:07 -0700

    Fix assistance_requests table

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -10,6 +10,7 @@ CREATE TABLE assistance_requests(
   id SERIAL PRIMARY KEY NOT NULL,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   created_at TIMESTAMP,
   started_at TIMESTAMP,
   completed_at TIMESTAMP,
